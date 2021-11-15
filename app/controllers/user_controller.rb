cdd75e0976a5e688a5f67c311de83905ccf041a1
Ubuntu <ubuntu@ip-172-31-36-16.ec2.internal>
2021-11-15 08:04:51 +0000

    controller edit(2)

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,20 +1,29 @@
 class UserController < ApplicationController
     def index
-        
+        @users = User.all
     end
     def new
-        
+        @user = User.new
     end
     def create
-        
+        @user = User.new(email: params[:user][:email], pass: params[:user][:pass], belong: params[:user][:belong])
+        if @user.save
+            redirect_to '/top/login_form'
+        else
+            render 'new'
+        end
     end
     def edit
-        
+        @user = User.find(params[:id])
     end
     def update
-        
+        user = User.find(params[:id])
+        user.update(email: params[:user][:email], pass: params[:user][:pass], belong: params[:user][:belong])
+        redirect_to '/users'
     end
     def destroy
-        
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to '/users'
     end
 end
