cc78364c8f12941a6f822332b5caad495db06c21
Ubuntu <ubuntu@ip-172-31-36-16.ec2.internal>
2021-11-22 07:09:16 +0000

    routes edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+    resources :user, :article, :store
+    root 'top#main'
+    get 'top/main', to: 'top#main'
+    post 'top/login'
+    get 'top/logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
